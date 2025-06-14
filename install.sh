a3f728d9060bd325d8df88722876ee1e7a5dfc40
Aditya8912S <101286617+Adityavihaan@users.noreply.github.com>
2025-06-14 16:44:02 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo "Installing Corefetch"
+g++ main.cpp -o corefetch
+sudo mv corefetch /usr/bin/corefetch
+
+echo "Installation complete now you can run corefetch by typing "corefetch" in the terminal. Thanks for using Corefetch"
